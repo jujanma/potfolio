6ef2cac6aad4ebb606f55063a1f9a9c280d766b5
Juan Manuel Marin <juanchomanuel289@gmail.com>
2024-11-07 13:29:23 -0500

    correccion enrutado Juanma dev

--- components/header.tsx
+++ components/header.tsx
@@ -1,33 +1,39 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import MotionTransition from "./transition";
 import { socialNetworks } from "@/data";
 
-
 const Header = () => {
-    return ( 
-        <MotionTransition position="bottom" className="absolute z-40 inline-block w-full top-5 md:top-10 px-20">
-            <header>
-                <div className="container justify-between max-w-6xl mx-auto md:flex">
-                    <Link href="/home">
-                    <h1 className="my-3 text-4xl font-bold text-center md:text-left">
-                        JuanMa
-                        <span className="text-secondary">
-                            Dev
-                        </span>
-                    </h1>                        
-                    </Link>
-                    <div className="flex items-center justify-center gap-7">
-                        {socialNetworks.map(({logo, src, id})=>(
-                            <Link key={id} href={src} target="_blank" className="transition-all duration-300 hover:text-secondary">{logo}</Link>
-                        ))}
+  return (
+    <MotionTransition
+      position="bottom"
+      className="absolute z-40 inline-block w-full top-5 md:top-10 px-20"
+    >
+      <header>
+        <div className="container justify-between max-w-6xl mx-auto md:flex">
+          <Link href="/">
+            <h1 className="my-3 text-4xl font-bold text-center md:text-left">
+              JuanMa
+              <span className="text-secondary">Dev</span>
+            </h1>
+          </Link>
+          <div className="flex items-center justify-center gap-7">
+            {socialNetworks.map(({ logo, src, id }) => (
+              <Link
+                key={id}
+                href={src}
+                target="_blank"
+                className="transition-all duration-300 hover:text-secondary"
+              >
+                {logo}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </header>
+    </MotionTransition>
+  );
+};
 
-                    </div>
-                </div>
-            </header>
-        </MotionTransition>
-     );
-}
- 
 export default Header;
